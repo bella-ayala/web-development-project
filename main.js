555d0ccd009eaf70296dc7939fd8c32ae10e47de
Isabella Ayala <122339370+bella-ayala@users.noreply.github.com>
2024-03-03 21:16:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-function displayTime() {
-    // Local time
+function displayTime() { //to get different timezones
    
 
-    const localDate = new Date().toLocaleString("en-US", {timeZone: "Asia/Bahrain"});
-    document.getElementById("localTime").innerHTML = localDate;
+    const bahrainDate = new Date().toLocaleString("en-US", {timeZone: "Asia/Bahrain"}); //creates the date and time in Bahrain
+    document.getElementById("bahrainTime").innerHTML = bahrainDate; //this is what I call in HTML for each different timezone
   
-    // Time in New York (Eastern Time Zone)
-    const nyDate = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
-    document.getElementById("nyTime").innerHTML = nyDate;
-  
-    // Time in London (GMT)
-    const londonDate = new Date().toLocaleString("en-US", {timeZone: "Europe/London"});
-    document.getElementById("londonTime").innerHTML = londonDate;
+    const jeddahDate = new Date().toLocaleString("en-US", {timeZone: "Asia/Riyadh"});//creates the date and time in Jeddah
+    document.getElementById("jeddahTime").innerHTML = jeddahDate;
+
+    const melbourneDate = new Date().toLocaleString("en-US", {timeZone: "Australia/Melbourne"});//creates the date and time in Melbourne
+    document.getElementById("melbourneTime").innerHTML = melbourneDate;
+
+    const tokyoDate = new Date().toLocaleString("en-US", {timeZone: "Asia/Tokyo"});//creates the date and time in Tokyo
+    document.getElementById("tokyoTime").innerHTML = tokyoDate;
+
+    const shanghaiDate = new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai"});//creates the date and time in Shanghai
+    document.getElementById("shanghaiTime").innerHTML = shanghaiDate;
+
+    const miamiDate = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});//creates the date and time in  Miami
+    document.getElementById("miamiTime").innerHTML = miamiDate;
+
+    const imolaDate = new Date().toLocaleString("en-US", {timeZone: "Europe/Rome"});//creates the date and time in Imola
+    document.getElementById("imolaTime").innerHTML = imolaDate;
+
+    const monacoDate = new Date().toLocaleString("en-US", {timeZone: "Europe/Rome"});//creates the date and time in Monaco
+    document.getElementById("monacoTime").innerHTML = monacoDate;
+
+    const montrealDate = new Date().toLocaleString("en-US", {timeZone: "America/Toronto"});//creates the date and time in Montreal
+    document.getElementById("montrealTime").innerHTML = montrealDate;
+
+    const catalunyaDate = new Date().toLocaleString("en-US", {timeZone: "Europe/Madrid"});//creates the date and time in Catalunya
+    document.getElementById("catalunyaTime").innerHTML = catalunyaDate;
+
+    const spielbergDate = new Date().toLocaleString("en-US", {timeZone: "Europe/Vienna"});//creates the date and time in Spielberg
+    document.getElementById("spielbergTime").innerHTML = spielbergDate;
+
+    const silverstoneDate = new Date().toLocaleString("en-US", {timeZone: "Europe/London"});//creates the date and time in Silverstone
+    document.getElementById("silverstoneTime").innerHTML = silverstoneDate;
+
+
   }
+
+
   
   // Update time every second
   setInterval(displayTime, 1000);
